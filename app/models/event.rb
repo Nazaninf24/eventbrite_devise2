6f26153b535c00370994ecc3da4553239e9895c3
Nazaninf24 <nazaninf24@gmail.com>
2020-02-20 17:54:35 +0100

    Fifth commit

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,44 @@
 class Event < ApplicationRecord
-end
+    validates :start_date, :duration, :title, :description, :price, :location, presence: :true
+    validate :cant_be_past
+    validate :duration_5_pos
+    validates :title, length: { in: 5..140}
+    validates :description, length: { in: 20..1000 }
+    validate :price_validation
+    belongs_to :admin, class_name: 'User'
+    has_many :attendances
+    has_many :users, through: :attendances
+  
+    def self.convert_date_time(date, time)
+      (date.to_s + " " +time.to_s).to_datetime
+    end
+  
+    private 
+    
+    def cant_be_past
+      if start_date.nil?
+        errors.add(:start_date, "must be filled") 
+      else
+        errors.add(:start_date, "cant be past") if start_date < Time.now
+      end
+    end
+    
+    def duration_5_pos
+      if duration.nil?
+        errors.add(:duration, "must be filled") 
+      else
+        errors.add(:duration, "has to be a multiple of 5") unless duration % 5 == 0
+        errors.add(:duration, "has to be a positive amount of minutes") unless duration > 0
+      end
+    end
+  
+    def price_validation
+      if price.nil?
+        errors.add(:price, "must be filled") 
+      else
+        errors.add(:price, "cant be free") if price < 1
+        errors.add(:price, "cant be over 1000") if price > 1000
+      end
+    end
+  end
+  
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
+
 Rails.application.routes.draw do
+  get 'events/show'
+  get 'events/new'
+  get 'events/index'
+  root 'events#index'
   devise_for :users
-  root to: "home#index"
+  resources :events, except: [:destroy, :edit, :update]
+  resources :users, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
